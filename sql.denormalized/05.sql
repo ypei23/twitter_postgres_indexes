68d8daa91a13f3c17dec954ef9ebe996ab4bb4d7
Yuechen.Pei <Yuechen.Pei@cmc.edu>
2024-05-01 01:17:35 -0700

    n

--- sql.denormalized/05.sql
+++ sql.denormalized/05.sql
@@ -1,8 +1,11 @@
-select tag, count(*) as count 
-from(
-	data->>'id' as id_tweets, '#' || (jsonb_array_elements(data->'entities'->'hashtags' || coalesce(data->'extended_tweet'->'entities'->'hashtags','[]'))->>'text') as tag 
-	from tweets_jsonb 
-	where to_tsvector('english', coalesce(data->'extended_tweet'->>'full_text',data->>'text')) @@ to_tsquery('english', 'coronavirus') and data->>'lang' = 'en') t1 
-group by tag
-order by count desc, tag
-limit 1000;
+SELECT tag, COUNT(*) AS count
+FROM (
+    SELECT data->>'id' AS id_tweets, '#' || (jsonb_array_elements(data->'entities'->'hashtags' || coalesce(data->'extended_tweet'->'entities'->'hashtags','[]'))->>'text') AS tag
+    FROM tweets_jsonb
+    WHERE to_tsvector('english', coalesce(data->'extended_tweet'->>'full_text', data->>'text')) @@ to_tsquery('english', 'coronavirus')
+        AND data->>'lang' = 'en'
+) AS t1
+GROUP BY tag
+ORDER BY count DESC, tag
+LIMIT 1000;
+
